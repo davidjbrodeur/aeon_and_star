7ea673ce5101d78bfb2d3a2cb7581c3e57a9a163
dbrodeur <davidjbrodeur@gmail.com>
2023-03-13 19:32:11 -0400

    reworked structure. created the game card for steel legion.

--- public/scripts/frontend.js
+++ public/scripts/frontend.js
@@ -13,3 +13,20 @@ setInterval(() => {
 
 const currentYear = new Date().getFullYear();
 document.getElementById("currentYear").textContent = currentYear;
+
+// Get the back-to-top button element
+const backToTopButton = document.querySelector("#back-to-top");
+
+// Function to check if the user has scrolled past the first screen
+function checkScrollPosition() {
+  const firstScreenHeight = window.innerHeight;
+  const scrollPosition = window.scrollY;
+  if (scrollPosition >= firstScreenHeight) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+// Add an event listener to the window object to check the scroll position
+window.addEventListener("scroll", checkScrollPosition);
